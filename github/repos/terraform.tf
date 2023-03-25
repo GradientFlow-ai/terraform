b4cc6583c48b78246827c9a638b370d65d3fab58
Timothy Roy <timothy.roy@protonmail.ch>
2023-03-25 15:17:21 -0500

    Add required checks

--- github/repos/terraform.tf
+++ github/repos/terraform.tf
@@ -4,7 +4,7 @@ resource "github_repository" "terraform" {
   description = "IaC for GradientFlow"
   visibility  = "public"
 
-  topics      = ["terraform", "iac", "embeddings"]
+  topics = ["terraform", "iac", "embeddings"]
 
   allow_merge_commit = true
   allow_rebase_merge = false
@@ -17,20 +17,19 @@ resource "github_repository" "terraform" {
 
 # Configure branch protection for the repository
 resource "github_branch_protection" "terraform" {
-  repository_id     = github_repository.terraform.name
-  pattern           = "main"
-  enforce_admins    = true
+  repository_id  = github_repository.terraform.name
+  pattern        = "main"
+  enforce_admins = true
 
   # We are using GH actions, provide action names as list of strings
- required_status_checks {
+  required_status_checks {
     strict   = false
-    contexts = ["Terraform validate"]
-    pull_request_bypassers = ["/coyotespike"] # until we have more reviewers :)
+    contexts = ["Validate terraform", "Check file formatting"]
   }
 
   # A new commit requires fresh approval
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
+    dismiss_stale_reviews  = true
     pull_request_bypassers = ["/coyotespike"] # until we have more reviewers :)
   }
 }
