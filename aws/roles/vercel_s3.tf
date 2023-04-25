60b8790b4cf1a95bace0dc039f5aba6c20036b40
coyotespike <timothy.roy@protonmail.ch>
2023-04-25 18:41:58 -0500

    Add new role and pass keys to Vercel

---
+++ aws/roles/vercel_s3.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_user" "vercel_s3_user" {
+  name = "VercelS3User"
+}
+
+resource "aws_iam_access_key" "vercel_s3_user_key" {
+  user = aws_iam_user.vercel_s3_user.name
+}
+
+resource "aws_iam_policy" "vercel_s3_policy" {
+  name        = "VercelS3Policy"
+  description = "Policy for Vercel to create pre-signed URLs for S3 access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:ListBucket"
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::${var.S3_BUCKET_NAME}",
+          "arn:aws:s3:::${var.S3_BUCKET_NAME}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "vercel_s3_policy_attachment" {
+  policy_arn = aws_iam_policy.vercel_s3_policy.arn
+  user       = aws_iam_user.vercel_s3_user.name
+}
+
+output "vercel_s3_user_access_key" {
+  value     = aws_iam_access_key.vercel_s3_user_key.id
+  sensitive = true
+}
+
+output "vercel_s3_user_secret_key" {
+  value     = aws_iam_access_key.vercel_s3_user_key.secret
+  sensitive = true
+}
+
+variable "S3_BUCKET_NAME" {
+  description = "The name of the S3 bucket, from the secrets file"
+  type        = string
+}
--- submodules.tf
+++ submodules.tf
@@ -12,17 +12,19 @@ module "s3" {
   source = "./aws/s3"
 }
 module "roles" {
-  source = "./aws/roles"
+  source         = "./aws/roles"
+  S3_BUCKET_NAME = var.S3_BUCKET_NAME
 }
 module "vercel" {
   source = "./vercel"
 
-  S3_BUCKET_NAME        = var.S3_BUCKET_NAME
-  AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
-  GITHUB_ID             = var.GITHUB_ID
-  GITHUB_SECRET         = var.GITHUB_SECRET
-  GOOGLE_CLIENT_ID      = var.GOOGLE_CLIENT_ID
-  GOOGLE_CLIENT_SECRET  = var.GOOGLE_CLIENT_SECRET
-  NEXTAUTH_URL          = var.NEXTAUTH_URL
+  s3_user_access_key_id     = module.roles.vercel_s3_user_access_key
+  s3_user_secret_access_key = module.roles.vercel_s3_user_secret_key
+
+  S3_BUCKET_NAME       = var.S3_BUCKET_NAME
+  GITHUB_ID            = var.GITHUB_ID
+  GITHUB_SECRET        = var.GITHUB_SECRET
+  GOOGLE_CLIENT_ID     = var.GOOGLE_CLIENT_ID
+  GOOGLE_CLIENT_SECRET = var.GOOGLE_CLIENT_SECRET
+  NEXTAUTH_URL         = var.NEXTAUTH_URL
 }
