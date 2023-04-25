262746f79049e47eaad18739fc6b7f2e0b387914
coyotespike <timothy.roy@protonmail.ch>
2023-04-25 14:33:35 -0500

    Remove TF vars from repo secrets as TF can access directly

--- github/repos/terraform.tf
+++ github/repos/terraform.tf
@@ -42,44 +42,22 @@ resource "github_actions_secret" "super_github_token" {
   secret_name     = "SUPER_GITHUB_TOKEN"
   plaintext_value = var.SUPER_GITHUB_TOKEN
 }
-resource "github_actions_secret" "tf_var_aws_access_key_id" {
+resource "github_actions_secret" "tf_var_aws_account_id" {
   repository      = "terraform"
-  secret_name     = "TF_VAR_AWS_ACCESS_KEY_ID"
-  plaintext_value = var.TF_VAR_AWS_ACCESS_KEY_ID
-}
-resource "github_actions_secret" "tf_var_aws_secret_access_key" {
-  repository      = "terraform"
-  secret_name     = "TF_VAR_AWS_SECRET_ACCESS_KEY"
-  plaintext_value = var.TF_VAR_AWS_SECRET_ACCESS_KEY
-}
-resource "github_actions_secret" "vercel_api_token" {
-  repository      = "terraform"
-  secret_name     = "VERCEL_API_TOKEN"
-  plaintext_value = var.VERCEL_API_TOKEN
+  secret_name     = "AWS_ACCOUNT_ID"
+  plaintext_value = var.TF_VAR_AWS_ACCOUNT_ID
 }
 
-
-variable "TF_VAR_AWS_ACCESS_KEY_ID" {
+## The below is duplicated from root/variables.tf
+variable "TF_VAR_AWS_ACCOUNT_ID" {
   type        = string
-  description = "Allows Terraform to manage S3. Created in roles/s3_iam"
-  default     = "secret"
-  sensitive   = true
-}
-variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {
-  type        = string
-  description = "Allows Terraform to manage S3. Created in roles/s3_iam"
+  description = "Allows GitHub runner to authenticate with AWS. Role created manually on AWS."
   default     = "secret"
   sensitive   = true
 }
 variable "SUPER_GITHUB_TOKEN" {
   type        = string
   description = "A second token necessary for Github Actions terraform plan and apply in Terraform repo"
-  default     = "secret"
-  sensitive   = true
-}
-variable "VERCEL_API_TOKEN" {
-  type        = string
-  description = "Allows Terraform's Vercel provider to manage Vercel projects"
   default     = "secret"
   sensitive   = true
 }
